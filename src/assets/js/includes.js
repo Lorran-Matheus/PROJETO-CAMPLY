d4bd20d638a29339c9e543f084a9e7cff0b92a81
Lorran-Matheus <lorranmatheus940@gmail.com>
2025-08-20 17:06:45 -0300

    fix includes.js "Referenciar layout mobile e desktop com base na página atual "

--- src/assets/js/includes.js
+++ src/assets/js/includes.js
@@ -1,31 +1,51 @@
 
-// Atribui o componente com base no tipo de tela desktop ou mobile
+
 function includeComponent(location, desktopPath, mobilePath) {
   const isMobile = window.matchMedia("(max-width: 768px)").matches;
-  const path = isMobile ? mobilePath : desktopPath;
+  const hash = window.location.hash.toLowerCase();
+
+  const page = document.querySelector(".pageLayout");
+  let path;
+
+  if (hash === '#login-a-index') {
+    path = isMobile ? mobilePath : desktopPath;
+  }
+
+  else if (isMobile && hash.startsWith('#login')) {
+    if (page) {
+      page.classList.remove("pageLayout");
+      page.innerHTML = "";
+    }
+    return;
+
+  } else {
+    path = desktopPath;
+  }
 
   fetch(path)
     .then(response => {
-      if (!response.ok) throw new Error("Erro ao carregar componente");
+      if (!response.ok) throw new Error(`Erro ao carregar componente: ${path}`);
       return response.text();
     })
     .then(html => {
-      const container = document.getElementById(location);
-      if (container) {
-        container.innerHTML = html;
-      }
+      const container = document.getElementsByClassName(location)[0];
+      if (container) container.innerHTML = html;
     })
     .catch(error => {
       console.error(`Erro ao carregar componente: ${path}`, error);
     });
+
 }
 
 document.addEventListener("DOMContentLoaded", () => {
+  const hash = window.location.hash.toLowerCase();
+  if (hash.startsWith('#login')) {
   includeComponent(
     "pageLayout",
     "src/components/loginLayoutDesktop.html",
     "src/components/loginLayoutMobile.html"
   );
+}
 
   includeComponent(
     "navbar",
@@ -41,6 +61,7 @@ window.addEventListener("resize", () => {
   clearTimeout(resizeTimeout);
 
   resizeTimeout = setTimeout(() => {
+    window.location.reload();
     includeComponent(
       "pageLayout",
       "src/components/loginLayoutDesktop.html",
